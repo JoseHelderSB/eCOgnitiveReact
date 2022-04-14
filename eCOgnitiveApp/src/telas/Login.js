aee595949a8928033f1ed6a948c0a4fd57b1f3e6
José Helder <joseheldersb@gmail.com>
2022-04-14 00:16:14 -0300

    adicionando botao para esconder e mostrar senha

--- eCOgnitiveApp/src/telas/Login.js
+++ eCOgnitiveApp/src/telas/Login.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Image, StyleSheet, Dimensions, TouchableOpacity, View, TextInput } from 'react-native';
+import { Ionicons} from '@expo/vector-icons';
 
 import Texto from '../componentes/Texto';
 
@@ -14,6 +15,10 @@ const width = Dimensions.get('screen').width;
 
 
 export default function Login(){
+    const [input, setInput] = useState('');
+    const [hidePass, setHidePass] =  useState(true);
+
+
     return (
         <>
         <View style={estilos.topo}>
@@ -25,28 +30,35 @@ export default function Login(){
 
 
        <View style={estilos.boxLogin}>
+
+
             <Texto style={estilos.emailSenha}>E-MAIL</Texto>
-            <View style={estilos.loginSenha}>
+            <View style={estilos.inputArea}>
                 <TextInput
                 style={estilos.input}
                 placeholder="Digite seu e-mail"
                 />
-            </View>
+            </View>  
 
-            <Texto style={estilos.emailSenha}>SENHA</Texto>
+            <Texto style={estilos.emailSenha} >SENHA</Texto>
 
-            <View>
-                <View style={estilos.loginSenha}>
-                    <TextInput
-                    style={estilos.input}
-                    secureTextEntry={true}
-                    placeholder="Digite sua senha"
-                    />
-                    <TouchableOpacity>
+            <View style={estilos.inputArea}>
+                <TextInput
+                style={estilos.input}
+                placeholder="Digite sua senha"
+                value={input}
+                onChangeText={(texto) => setInput(texto)}
+                secureTextEntry={hidePass}
+                 />
 
-                    </TouchableOpacity>
-                </View>
-            </View>
+                <TouchableOpacity style={estilos.icon} onPress={() => setHidePass(!hidePass)}>
+                    { hidePass ?
+                        <Ionicons name="eye" size={20}/>
+                        :
+                        <Ionicons name="eye-off" size={20}/>
+                    }
+               </TouchableOpacity>
+             </View>
 
         </View>
 
@@ -55,11 +67,15 @@ export default function Login(){
 }
 
 const estilos = StyleSheet.create({
+
     container: {
-        flex:1,
-        justifyContent: 'center',
+        flex: 1,
         alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 20,
+        width: "90%",
     },
+
     topo:{
         flexDirection: "row",
         paddingVertical: 3,
@@ -98,16 +114,31 @@ const estilos = StyleSheet.create({
         marginTop: 20,
     },
 
+    inputArea: {
+        flexDirection: 'row',
+        width: '98%',
+        alignItems: 'center',
+        alignItems: 'center',
+        marginLeft: 35,
+    },
+
     input: {
         borderWidth: 1,
         borderRadius:5,
-        width: 330,
+        width: "85%",
         height: 40,
         padding: 8,
+        
     },
 
     boxLogin: {
         marginTop: 40,
+    },
+
+    icon: {
+        position: 'absolute',
+        marginLeft: 290,
+        width: "15%",
     },
     
     
